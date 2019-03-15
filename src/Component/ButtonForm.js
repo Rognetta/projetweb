36873c1d30af79e951d949b0d36df1e763cd0e54
Rognetta <aurelio.rognetta@edu.ece.fr>
2019-03-15 17:40:11 +0100

    Update ButtonForm.js
    Router

--- src/Component/ButtonForm.js
+++ src/Component/ButtonForm.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {BrowserRouter} from "react-router-dom";
 
 // The ButtonForm creates links that can be used to navigate between routes.
 const ButtonForm = () => (
     <header>
+        <BrowserRouter>
         <div className="commonBackground">
             <h1>
                 <Link to="/artist">Artist Form</Link>
@@ -11,6 +13,7 @@ const ButtonForm = () => (
                 <Link to="/track">Track Form</Link>
             </h1>
         </div>
+        </BrowserRouter>
     </header>
 )
 
